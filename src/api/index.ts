89e3da3d1c93b86ef49847ed3a4f4f57959dcc7f
DragonLJH <1183665002@qq.com>
2022-10-17 09:29:22 +0800

    api位置调动

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,78 +4,74 @@ import type { AxiosRequestConfig } from 'axios'
 const axios = myAxios();
 
 
-export const queryProductByEmergeApi = (url: string, config: AxiosRequestConfig) => {
-    return axios.get(url, config)
-
-}
-export const queryShopByUserNameApi = (url: string, config: AxiosRequestConfig) => {
-    return axios.get(url, config)
-
-}
-export const updateShopByIdApi = (url: string, config: AxiosRequestConfig) => {
-    return axios.post(url, config)
-
-}
-export const deleteShopbyIdApi = (url: string, config: AxiosRequestConfig) => {
-    return axios.get(url, config)
-
-}
+/*-----------------------------------------User-------------------------------------------------------*/
 export const queryUserByUserNameApi = (url: string, config: AxiosRequestConfig) => {
     return axios.get(url, config)
-
 }
 export const insertUserApi = (url: string, config: AxiosRequestConfig) => {
     return axios.post(url, config)
-
-
 }
+/*-----------------------------------------User-------------------------------------------------------*/
+
+/*-----------------------------------------Order-------------------------------------------------------*/
 export const queryAllOrderByUserNameApi = (url: string, config: AxiosRequestConfig) => {
     return axios.get(url, config)
+}
 
+/*-----------------------------------------Order-------------------------------------------------------*/
 
+/*-----------------------------------------Type-------------------------------------------------------*/
+export const queryAllTypeApi = (url: string, config: AxiosRequestConfig) => {
+    return axios.get(url, config)
+}
+/*-----------------------------------------Type-------------------------------------------------------*/
+
+/*-----------------------------------------Rotation-------------------------------------------------------*/
+export const queryAllRotationApi = (url: string, config: AxiosRequestConfig) => {
+    return axios.get(url, config)
+}
+/*-----------------------------------------Rotation-------------------------------------------------------*/
+
+/*-----------------------------------------product-------------------------------------------------------*/
+export const queryProductByTypeApi = (url: string, config: AxiosRequestConfig) => {
+    return axios.get(url, config)
+}
+export const queryProductByEmergeApi = (url: string, config: AxiosRequestConfig) => {
+    return axios.get(url, config)
 }
 export const queryProductByIdApi = (url: string, config: AxiosRequestConfig) => {
     return axios.get(url, config)
-
-
 }
 export const queryCommentsByProductIdApi = (url: string, config: AxiosRequestConfig) => {
     return axios.get(url, config)
-
-
-}
-export const insertShopApi = (url: string, config: AxiosRequestConfig) => {
-    return axios.get(url, config)
-
-
 }
 export const queryProductByProductMsgApi = (url: string, config: AxiosRequestConfig) => {
     return axios.get(url, config)
-
-
 }
-export const queryAllTypeApi = (url: string, config: AxiosRequestConfig) => {
-    return axios.get(url, config)
+/*-----------------------------------------product-------------------------------------------------------*/
 
 
-}
-export const queryAllRotationApi = (url: string, config: AxiosRequestConfig) => {
-    return axios.get(url, config)
-
-
-}
+/*-----------------------------------------emerge-------------------------------------------------------*/
 export const queryAllEmergeApi = (url: string, config: AxiosRequestConfig) => {
     return axios.get(url, config)
-
-
 }
-export const queryProductByTypeApi = (url: string, config: AxiosRequestConfig) => {
+/*-----------------------------------------emerge-------------------------------------------------------*/
+
+
+/*-----------------------------------------shop-------------------------------------------------------*/
+export const queryShopByUserNameApi = (url: string, config: AxiosRequestConfig) => {
     return axios.get(url, config)
-
-
 }
-
-
+export const updateShopByIdApi = (url: string, config: AxiosRequestConfig) => {
+    return axios.post(url, config)
+}
+export const deleteShopbyIdApi = (url: string, config: AxiosRequestConfig) => {
+    return axios.get(url, config)
+}
+export const insertShopApi = (url: string, config: AxiosRequestConfig) => {
+    return axios.get(url, config)
+}
+/*-----------------------------------------shop-------------------------------------------------------*/
 
 
 
