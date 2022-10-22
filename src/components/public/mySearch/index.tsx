023d8cdab75e97e4e8016607d7e93c656bf3004c
DragonLJH <1183665002@qq.com>
2022-10-22 10:13:05 +0800

    查询样式修改

--- src/components/public/mySearch/index.tsx
+++ src/components/public/mySearch/index.tsx
@@ -1,7 +1,8 @@
 import { FC, useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import { Row, Col, Divider, Input } from 'antd';
-import { ShoppingCartOutlined } from '@ant-design/icons'; 
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import "./css/index.css"
 const { Search } = Input;
 
 const MySearch: FC = () => {
@@ -16,15 +17,37 @@ const MySearch: FC = () => {
         localStorage.setItem("myLoclSearch", value)
         history.replace({ pathname: "/searchPage", state: { value } });
     }
+    const [inputVal, setInputVal] = useState("")
+    const onClick = () => {
+        localStorage.setItem("myLoclSearch", inputVal)
+        history.replace({ pathname: "/searchPage", state: { value: inputVal } });
+        setInputVal("")
+    }
+    const mySearchDivMsg = ["男鞋", "女鞋", "男衬衫", "女卫衣", "包包", "背包"]
     return (
         <>
             {pathname === "/loginPage" ? "" :
-                <Row gutter={40} style={{ backgroundColor: "#fff" }} align="bottom">
-                    <Col flex="300px" ><Divider style={{ cursor: "pointer" }}> <span onClick={() => { history.replace({ pathname: "/" }); }}> DragonLogo</span> </Divider></Col>
-                    <Col flex="auto">
-                        <Search placeholder="input search text" enterButton="Search" size="large" onSearch={onSearch} />
+                <Row gutter={40} style={{ backgroundColor: "#fff" }} align="middle">
+                    <Col flex="300px" >
+                        <Divider style={{ cursor: "pointer" }}> <span onClick={() => { history.replace({ pathname: "/" }); }}> DragonLogo</span> </Divider>
                     </Col>
-                    <Col flex="300px" ><Divider style={{ cursor: "pointer" }}> <ShoppingCartOutlined /><span onClick={() => { history.replace({ pathname: "/shoppingCartPage" }); }}> 购物车</span> </Divider></Col>
+                    <Col flex="auto">
+                        {/* <Search placeholder="请输入查询信息" enterButton="Search" size="large" onSearch={onSearch} /> */}
+                        <div className="my-search-div">
+                            <div className="my-search-div-input">
+                                <input type="text" value={inputVal} onInput={(e: any) => { setInputVal(e.target.value) }} />
+                                <span className="my-search-div-input-button" onClick={onClick}>搜索</span>
+                            </div>
+                            <div className="my-search-div-msg">
+                                {mySearchDivMsg.map((item, index) => {
+                                    return <span key={index} className="item">{item}</span>
+                                })}
+                            </div>
+                        </div>
+                    </Col>
+                    <Col flex="300px" >
+                        <Divider style={{ cursor: "pointer" }}> <ShoppingCartOutlined /><span onClick={() => { history.replace({ pathname: "/shoppingCartPage" }); }}> 购物车</span> </Divider>
+                    </Col>
                 </Row>
             }
         </>
